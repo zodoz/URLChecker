69187b714e8411460cfa0cd6f49243c2b3b46c8c
Mike Kent <mike@DevSite.(none)>
2010-07-29 22:43:23 -0500

    removed my default website

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,6 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		new URLCheckManager("http://www.me.ttu.edu").start();
+		new URLCheckManager("http://site here").start();
 	}
 }
